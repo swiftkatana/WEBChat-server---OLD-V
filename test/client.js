ed4ae6dcf59af067e4f9b6d88af9ae1dc7abe327
Your Name <danial1029@hotmail.com>
2020-09-29 12:24:51 +0300

    ddsa

--- test/client.js
+++ test/client.js
@@ -3,7 +3,19 @@ navigator.getUserMedia = (navigator.getUserMedia || navigator.webkitGetUserMedia
 let constraintsA = {audio:true,video:false};
 let videoArea = document.querySelector("video");
 let audioArea = document.querySelector("audio");
-let constraintsV = {audio:true,video:true};
+let constraintsV = 
+{
+   audio:true,
+   video:{
+      mandatory:{
+         minWidth:640,
+         maxWidth:640,
+         minHeight:360,
+         maxHeight:480
+      }
+   },
+
+};
 
   function onSuccessV(stream){
    console.log('success Video');
@@ -25,7 +37,6 @@ let constraintsV = {audio:true,video:true};
   
 if (navigator.getUserMedia) {
   navigator.getUserMedia(constraintsV,onSuccessV,onError);
- navigator.getUserMedia(constraintsA,onSuccessA,onError);
 
 } else { 
    alert("WebRTC is not supported"); 
