12eef6550a0cc5e1457145c4198e41b0a2270356
Your Name <danial1029@hotmail.com>
2020-09-29 03:33:14 +0300

    dsa

--- test/client.js
+++ test/client.js
@@ -1,23 +1,29 @@
+navigator.getUserMedia = (navigator.getUserMedia || navigator.webkitGetUserMedia || 
+   navigator.mozGetUserMedia); 
+let constraintsA = {audio:true,video:false};
+let videoArea = document.querySelector("video");
+let audioArea = document.querySelector("audio");
+let constraintsV = {audio:false,video:true};
 
-  function onSuccess(stream){
-   console.log('success connect');
+  function onSuccessV(stream){
+   console.log('success Video');
    videoArea.src=window.URL.createObjectURL(stream);
    videoArea.play();
    }
-     
+   function onSuccessA(stream){
+      console.log('success audioArea');
+      audioArea.src=window.URL.createObjectURL(stream);
+      audioArea.play();
+      }
    function onError(e){
       console.log(e)
    
    }
 
   
-   navigator.getUserMedia = (navigator.getUserMedia || navigator.webkitGetUserMedia || 
-      navigator.mozGetUserMedia); 
-let constraints = {audio:true,video:false};
-let videoArea = document.querySelector("video");
- 
 if (navigator.getUserMedia) {
-  navigator.getUserMedia(constraints,onSuccess,onError);
+  navigator.getUserMedia(constraintsV,onSuccessV,onError);
+  navigator.getUserMedia(constraintsA,onSuccessA,onError);
 
 } else { 
    alert("WebRTC is not supported"); 
