3e6a235b424213dafb55896eeb84bb53e2dd5d4b
Your Name <danial1029@hotmail.com>
2020-09-29 12:38:00 +0300

    da

--- test/client.js
+++ test/client.js
@@ -1,43 +1,73 @@
-navigator.getUserMedia = (navigator.getUserMedia || navigator.webkitGetUserMedia || 
-   navigator.mozGetUserMedia); 
+
 let constraintsA = {audio:true,video:false};
 let videoArea = document.querySelector("video");
 let audioArea = document.querySelector("audio");
-let constraintsV = 
-{
-   audio:true,
-   video:{
-      mandatory:{
-         minWidth:640,
-         maxWidth:640,
-         minHeight:360,
-         maxHeight:480
+let videoSelect = document.querySelector("#camera");
+
+
+
+
+function startStream(){
+   navigator.getUserMedia = (navigator.getUserMedia || navigator.webkitGetUserMedia || 
+      navigator.mozGetUserMedia); 
+      let constraintsV = 
+      {
+         audio:true,
+         video:{
+            mandatory:{
+               minWidth:640,
+               maxWidth:640,
+               minHeight:360,
+               maxHeight:480
+            }
+         },
+         
+      };
+      
+      if (navigator.getUserMedia) {
+         navigator.getUserMedia(constraintsV,onSuccessV,onError);
+         
+      } else { 
+         alert("WebRTC is not supported"); 
+    }
+    
+    
+   }
+   function getCameras(sourceInfos){
+
+      for (let i = 0; i < sourceInfos.length; i++) {
+            let sourceInfo = sourceInfos[i];
+            var option = document.createElement('option')
+            option.value = sourceInfo.id;
+            if(sourceInfo.kind==='video'){
+               option.text=sourceInfo.label ||'camera'+(videoSelect.length+1);
+               videoSelect.appendChild(option);
+            }
+            
+
+
       }
-   },
 
-};
-
-  function onSuccessV(stream){
-   console.log('success Video');
-   videoArea.srcObject=stream;
-   videoArea.play();
+   }
+   
+   function onSuccessV(stream){
+      console.log('success Video');
+      videoArea.srcObject=stream;
+      videoArea.play();
    }
    function onSuccessA(stream){
       console.log('success audioArea');
       
       audioArea.srcObject=stream;
-
-   audioArea.play();
-      }
+      
+      audioArea.play();
+   }
    function onError(e){
       console.log(e)
       navigator.getUserMedia(constraintsA,onSuccessA,onError);
    }
-
-  
-if (navigator.getUserMedia) {
-  navigator.getUserMedia(constraintsV,onSuccessV,onError);
-
-} else { 
-   alert("WebRTC is not supported"); 
-}
+   
+   
+   startStream()
+   MediaStreamTrack.getSources(getCameras)
+   
