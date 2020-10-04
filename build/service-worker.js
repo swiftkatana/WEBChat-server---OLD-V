5972f0d5fa8dd30077f5b07675fffc2bc81654c1
Your Name <danial1029@hotmail.com>
2020-10-04 22:13:04 +0300

    5

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.9e8b817876f13284967fc26e3e28d72c.js"
+  "/precache-manifest.6fe57778fa00e4da7e1f1324ed748f46.js"
 );
 
 self.addEventListener('message', (event) => {
